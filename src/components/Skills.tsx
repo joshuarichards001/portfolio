2076606b2c53b1bc4b79c06a2b8900accae80a02
josh richards <joshuarichards001@gmail.com>
2020-11-29 16:02:25 +1300

    font changes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,34 +3,29 @@ import styles from "../Main.module.css";
 
 export default function Skills(): ReactElement {
   return (
-    <div style={{marginTop: '10px'}}>
+    <div style={{ marginTop: "10px" }}>
+      <h3 className={styles.category}>Skills</h3>
       <div className={styles.subExperience}>
-        <div>
-          <h3 className={styles.category}>Languages</h3>
-          <ul className={styles.skills}>
-            <li>C#</li>
-            <li>Python</li>
-            <li>Java</li>
-            <li>C++</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>SQL Server</li>
-          </ul>
-        </div>
-        <div>
-          <h3 className={styles.category}>Technologies</h3>
-          <ul className={styles.skills}>
-            <li>React</li>
-            <li>TypeScript</li>
-            <li>ASP.NET</li>
-            <li>Django</li>
-            <li>Git</li>
-            <li>Agile</li>
-            <li>React Native</li>
-            <li>AI/ML</li>
-          </ul>
-        </div>
+        <ul className={styles.skills}>
+          <li>C#</li>
+          <li>Python</li>
+          <li>Java</li>
+          <li>C++</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>SQL Server</li>
+        </ul>
+        <ul className={styles.skills}>
+          <li>React</li>
+          <li>TypeScript</li>
+          <li>ASP.NET</li>
+          <li>Django</li>
+          <li>Git</li>
+          <li>Agile</li>
+          <li>React Native</li>
+          <li>AI/ML</li>
+        </ul>
       </div>
     </div>
   );
