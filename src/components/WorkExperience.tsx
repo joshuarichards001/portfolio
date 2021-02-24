81f06d710c6dfd41510f474b77f6e784c9f115e6
josh richards <joshuarichards001@gmail.com>
2021-02-24 14:15:41 +1300

    Added VUW work experience

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,15 +1,17 @@
 import React, { ReactElement } from "react";
 import styles from "../Main.module.css";
 import CRAutomation from "../pages/CRAutomation";
+import Vuw from "../pages/Vuw";
 import Xero from "../pages/Xero";
 
 export default function Work(): ReactElement {
   return (
     <div className={styles.experience}>
-      <h3 className={styles.categoryTitle}>Work</h3>
+      <h3 className={styles.categoryTitle}>Work and Education</h3>
       <div className={styles.subExperience}>
         <Xero />
         <CRAutomation />
+        <Vuw />
       </div>
     </div>
   );
---
+++ src/pages/Vuw.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from "react";
+import Modal from "react-modal";
+import vuwImage from "../images/vuw.png";
+import newInvoicingImage from "../images/newInvoicing.png";
+import styles from "../Main.module.css";
+
+export default function Vuw(): ReactElement {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button className={styles.card} onClick={openModal}>
+        <img src={vuwImage} alt="Avatar" style={{ width: "100%" }} />
+        <div className={styles.cardContainer}>
+          <b>Victoria University of Wellington</b>
+          <p>Tutor / Class Rep / Student</p>
+        </div>
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Vuw"
+        className={styles.modalContent}
+        overlayClassName={styles.modalOverlay}
+      >
+        <h1>Victoria University of Wellington</h1>
+        <div className={styles.modalSubContent}>
+          <p>
+            I am currently completing my final year of Software Engineering at
+            Victoria University of Wellington. Over the course of my Software
+            Engineering degree I have filled a in a number of roles. Firstly I
+            am currently a tutor in the class SWEN301 of which goes over web
+            fundamentals. Additionally this semester I have been the class
+            representative for SWEN431 - Advanced programming languages. Click on the image to learn more about how VUW's Software Engineering Program is Structured.
+          </p>
+          <a
+            href="https://www.wgtn.ac.nz/explore/study-areas/software-and-computer-science/overview"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={vuwImage} alt="" />
+          </a>
+        </div>
+      </Modal>
+    </div>
+  );
+}
